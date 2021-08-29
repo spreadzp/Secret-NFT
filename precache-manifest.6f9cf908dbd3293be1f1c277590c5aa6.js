e098d51d07bce3983c6b12d337ea8ce4b3467f49
spreadzp <spread2009@gmail.com>
2021-08-29 11:16:15 +0300

    Updates

---
+++ precache-manifest.6f9cf908dbd3293be1f1c277590c5aa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48d90af8a18593625316e55e2c0c47f",
+    "url": "/Secret-NFT/index.html"
+  },
+  {
+    "revision": "6499d9c845ae5d0d2156",
+    "url": "/Secret-NFT/static/css/main.294e8144.chunk.css"
+  },
+  {
+    "revision": "9435c568e026dcff8069",
+    "url": "/Secret-NFT/static/js/2.9797668b.chunk.js"
+  },
+  {
+    "revision": "6499d9c845ae5d0d2156",
+    "url": "/Secret-NFT/static/js/main.497c334a.chunk.js"
+  },
+  {
+    "revision": "64cc53b293e60c2fffe1",
+    "url": "/Secret-NFT/static/js/runtime~main.a9c8cc16.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Secret-NFT/precache-manifest.402a961395f6d5bc9721323e9c80684e.js"
+  "/Secret-NFT/precache-manifest.6f9cf908dbd3293be1f1c277590c5aa6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Secret-NFT/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
